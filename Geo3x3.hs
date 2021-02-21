b8f802279feb41a785793635fab94c5ef5874297
mitsuji <tkms@mitsuji.org>
2021-02-21 17:57:23 +0900

    fix: haskell: decode: support trailing zero

--- Geo3x3.hs
+++ Geo3x3.hs
@@ -10,8 +10,7 @@ import qualified Data.ByteString.Lazy.Char8 as LB8
 import qualified Data.ByteString.Builder as Builder
 import qualified Control.Monad.RWS as RWS
 import qualified Control.Monad.State as State
-import Data.Monoid ((<>))
-import Control.Monad (forM_)
+import Control.Monad (forM_, when)
 import Data.Char (digitToInt)
 
 type Encoder =  RWS.RWS () Builder.Builder (Double,Double,Double) -- (lat,lng,unit)
@@ -27,8 +26,7 @@ encode' lat lng level = Builder.toLazyByteString $ snd $ RWS.evalRWS f () (lat,l
             then RWS.tell $ Builder.char7 'E'
             else do
               RWS.tell $ Builder.char7 'W'
-              (lat,lng,unit)<-RWS.get
-              RWS.put (lat,lng+180,unit)
+              RWS.modify' $ \(lat,lng,unit) -> (lat,lng+180,unit)
           (lat,lng,unit)<-RWS.get
           let lat' = 90 - lat  -- 0:the North Pole,  180:the South Pole
           RWS.put (lat',lng,180)
@@ -51,25 +49,29 @@ decode' code = State.execState f (0.0, 0.0, 0, 0.0)
       if B8.null code then error "trying to decode empty data"
       else do
         let c = B8.index code 0
-        let (begin,flg) = case c of
+            (begin,flg) = case c of
               _ | c == '-' || c == 'W' -> (1,True)
               _ | c == '+' || c == 'E' -> (1,False)
               _ -> (0,False)
-        let unit = 180
+            unit = 180
             lat = 0
             lng = 0
             level = 1
             clean = B8.length code
         State.put (lat,lng,level,unit)
-        forM_ [begin..clean-1] $ \i -> do
-          (lat,lng,level,unit) <- State.get 
-          let n = digitToInt $ B8.index code i
-              unit' = unit / 3
-              n' = n - 1
-              lng' = lng + (fromIntegral $ n' `mod` 3) * unit'
-              lat' = lat + (fromIntegral $ n' `div` 3) * unit'
-              level' = level +1
-          State.put (lat',lng',level',unit')
+        let loop = \i -> do
+            when (i < clean) $ do
+              let n = digitToInt $ B8.index code i
+              when (n > 0) $ do
+                (lat,lng,level,unit) <- State.get
+                let unit' = unit / 3
+                    n' = n - 1
+                    lng' = lng + (fromIntegral $ n' `mod` 3) * unit'
+                    lat' = lat + (fromIntegral $ n' `div` 3) * unit'
+                    level' = level +1
+                State.put (lat',lng',level',unit')
+              loop $ i + 1
+        loop begin
         (lat,lng,level,unit) <- State.get 
         let
           lat' = lat + unit / 2
