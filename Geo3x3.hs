3313d2c00bbcb28a8051c46900e188cc84f745ee
mitsuji <tkms@mitsuji.org>
2021-02-26 06:54:05 +0900

    haskell: coding style

--- Geo3x3.hs
+++ Geo3x3.hs
@@ -1,11 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
 
-module Geo3x3
-    ( encode'
-    , decode'
-    , encode
-    , decode
-    ) where
+module Geo3x3 ( encode'
+              , decode'
+              , encode
+              , decode
+              ) where
 
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as LB8
@@ -55,38 +54,37 @@ decode' :: B8.ByteString -> (Lat,Lng,Level,Unit)
 decode' code = State.execState f (0,0,1,180) -- (lat,lng,level,unit)
   where
     f :: Decoder ()
-    f = do
-      if B8.null code then error "trying to decode empty data"
-      else do
-
-        let (begin,isWest) =
-              case B8.index code 0 of
-                c | c == '-' || c == 'W' -> (1,True)
-                c | c == '+' || c == 'E' -> (1,False)
-                _ -> (0,False)
-
-        let loop = \i ->
-              when (i < B8.length code) $
-                let n = digitToInt $ B8.index code i
-                 in when (n > 0) $ do
-                   State.modify' $ \(lat,lng,level,unit) ->
-                     let !unit' = unit / 3
-                         (!lng',!lat') = n - 1 & \n ->
-                           (lng + (fromIntegral $ n `mod` 3) * unit',
-                            lat + (fromIntegral $ n `div` 3) * unit')
-                         !level' = level + 1
-                      in (lat',lng',level',unit')
-                   loop $ i + 1
-         in loop begin
-
-        State.modify' $ \(lat,lng,level,unit) ->
-          let !lat' = lat + unit / 2 & \lat -> 90 - lat
-              !lng' = lng + unit / 2 & \lng -> if isWest then lng - 180 else lng
-           in (lat',lng',level,unit)
+    f = if B8.null code then error "trying to decode empty data"
+        else do
+  
+          let (begin,isWest) =
+                case B8.index code 0 of
+                  c | c == '-' || c == 'W' -> (1,True)
+                  c | c == '+' || c == 'E' -> (1,False)
+                  _ -> (0,False)
+  
+          let loop = \i ->
+                when (i < B8.length code) $
+                  let n = digitToInt $ B8.index code i
+                   in when (n > 0) $ do
+                     State.modify' $ \(lat,lng,level,unit) ->
+                       let !unit' = unit / 3
+                           (!lng',!lat') = n - 1 & \n ->
+                             (lng + (fromIntegral $ n `mod` 3) * unit',
+                              lat + (fromIntegral $ n `div` 3) * unit')
+                           !level' = level + 1
+                        in (lat',lng',level',unit')
+                     loop $ i + 1
+           in loop begin
+  
+          State.modify' $ \(lat,lng,level,unit) ->
+            let !lat' = lat + unit / 2 & \lat -> 90 - lat
+                !lng' = lng + unit / 2 & \lng -> if isWest then lng - 180 else lng
+             in (lat',lng',level,unit)
 
 
 encode :: Double -> Double -> Int -> String
 encode lat lng level = LB8.unpack $ encode' lat lng level
 
-decode :: String -> (Double, Double, Int, Double)
+decode :: String -> (Double,Double,Int,Double)
 decode = decode' . B8.pack
