0e67c45fa5127c61622f9dcff978996b0d2e9893
mitsuji <tkms@mitsuji.org>
2021-02-26 13:50:47 +0900

    racket: refactoring

--- geo3x3.rkt
+++ geo3x3.rkt
@@ -1,122 +1,66 @@
 #lang racket
 
-
 (define (encode-bytes lat lng level)
-  (cond
-    [(< level 1) (error "invalid level")]
-    [else
-       (let ([unit null]
-             [res (make-bytes 0)])
-
-           (define (update lat0 lng0 unit0 c0)
-                   (set! lat lat0)
-                   (set! lng lng0)
-                   (set! unit unit0)
-                   (set! res (bytes-append res (bytes (char->integer c0))))
-           )
-
-           (
-             (lambda (clng)
-               (match clng
-                 [(list c lng) (update (- 90 lat) lng 180 c)]
-               )
-             )
-             (if (>= lng 0) (list #\E lng) (list #\W (+ lng 180)))
-           )
-
-           (for-each
-             (lambda (_)
-               (apply update (let* ([unit (/ unit 3)]
-                                    [x (exact-floor (/ lng unit))]
-                                    [y (exact-floor (/ lat unit))]
-                                    [c (integer->char (+ (char->integer #\0) x (* y 3) 1))]
-                                    [lng (- lng (* x unit))]
-                                    [lat (- lat (* y unit))])
-                                   (list lat lng unit c)
-                             )
-               )
-             )
-             (range 1 level)
-           ) 
-           res
-       )
-    ]
-  )
-)
-
+   (cond [(< level 1) (error "invalid level")]
+         [else
+             (let* ([state '()]
+                    [res (make-bytes 0)]
+                    [display (lambda (c) (set! res (bytes-append res (bytes (char->integer c)))))])
+                 ((match-lambda [(cons c lng)
+                                 (display c) (set! state (list (- 90 lat) lng 180))])
+                     (if (>= lng 0) (cons #\E lng)
+                                    (cons #\W (+ lng 180))))
+                 (for-each (lambda (_)(
+                       (match-lambda [(list lat lng unit)
+                                   (let* ([unit (/ unit 3)]
+                                          [x (exact-floor (/ lng unit))]
+                                          [y (exact-floor (/ lat unit))]
+                                          [c (integer->char (+ (char->integer #\0) x (* y 3) 1))]
+                                          [lng (- lng (* x unit))]
+                                          [lat (- lat (* y unit))])
+                                        (display c) (set! state (list lat lng unit)))])
+                            state))
+                      (range 1 level))
+                 res)]))
 
 (define (decode-bytes code)
-  (cond
-    [(or (not (bytes? code)) (= (bytes-length code) 0)) (error "trying to decode empty data")]
-    [else
-      (let ([begin null]
-            [is-west null])
-        (
-	  (lambda (c)
-            (cond
-               [(or (eq? c #\-) (eq? c #\W))
-                (set! begin 1)
-                (set! is-west #t)]
-               [(or (eq? c #\+) (eq? c #\E))
-                (set! begin 1)
-                (set! is-west #f)]
-               [else
-                (set! begin 0)
-                (set! is-west #f)]
-            )
-          ) (integer->char (bytes-ref code 0))
-        )
-
-        (let ([lat 0]
-              [lng 0]
-              [level 1]
-              [unit 180])
-          (define (loop clen i)
-            (when (< i clen)
-              (
-                (lambda (n)
-                  (when (> n 0)
-                        (set! unit (/ unit 3))
-                        (set! lng (+ lng (* (remainder (- n 1) 3) unit)))
-                        (set! lat (+ lat (* (quotient  (- n 1) 3) unit)))
-                        (set! level (+ level 1))
-                        (loop clen (+ i 1))
-                  )
-                ) (- (bytes-ref code i) (char->integer #\0))
-              )
-	    )
-          )
-          (loop (bytes-length code) begin)
-
-          (list
-            (exact->inexact ( (lambda (lat) (- 90 lat))
-                                      (+ lat (/ unit 2))
-                            ))
-            (exact->inexact ( (lambda (lng) (if is-west (- lng 180) lng))
-                                      (+ lng (/ unit 2))
-                            ))
-            level
-            (exact->inexact unit)
-          )
-        )
-
-      )
-    ]
-  )
-)
+   (cond [(or (not (bytes? code)) (= (bytes-length code) 0)) (error "trying to decode empty data")]
+         [else
+            (let-values ([(begin is-west) 
+                 (let ([c (integer->char (bytes-ref code 0))])
+                     (cond [(or (eq? c #\-) (eq? c #\W)) (values 1 #t)]
+                           [(or (eq? c #\+) (eq? c #\E)) (values 1 #f)]
+                           [else                         (values 0 #f)]))])
+                 (let ([state (list 0 0 1 180)]
+                       [clen (bytes-length code)])
+                     (let loop ((i begin))
+                         (when (< i clen)
+                             (let ([n (- (bytes-ref code i) (char->integer #\0))])
+                                 (when (> n 0)
+                                    (let ([n (- n 1)])
+                                       ((match-lambda [(list lat lng level unit)
+                                            (let* ([unit  (/ unit 3)]
+                                                   [lng   (+ lng (* (remainder n 3) unit))]
+                                                   [lat   (+ lat (* (quotient  n 3) unit))]
+                                                   [level (+ level 1)])
+                                                (set! state (list lat lng level unit))
+                                                (loop (+ i 1)))])
+                                         state))))))
+                     ((match-lambda [(list lat lng level unit)
+                             (list (exact->inexact (let ([lat (+ lat (/ unit 2))]) (- 90 lat) ))
+                                   (exact->inexact (let ([lng (+ lng (/ unit 2))]) (if is-west (- lng 180) lng) ))
+                                   level
+                                   (exact->inexact unit))])
+                         state)))]))
 
 
 ;(encode 35.65858 139.745433 14)
 (define (encode lat lng level)
-  (bytes->string/utf-8 (encode-bytes lat lng level))
-)
-
+    (bytes->string/latin-1 (encode-bytes lat lng level)))
 
 ;(decode "E3793653391822")
 (define (decode code)
-  (decode-bytes (string->bytes/utf-8 code))
-)
-
+    (decode-bytes (string->bytes/latin-1 code)))
 
 (provide encode-bytes)
 (provide decode-bytes)
