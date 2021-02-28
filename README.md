f4d89a2f651258d5c167e4a709a5a4e52b352095
Taisuke Fukuno <fukuno@jig.jp>
2021-02-28 11:01:26 +0900

    add PureScript

--- README.md
+++ README.md
@@ -22,9 +22,9 @@ divide the earth to two (West or East), recursive divisiton 3x3(9th). longer is 
 https://taisukef.github.io/Geo3x3/  
 
 ## Supported Languages
-36 programming languages supported now  
-現在36のプログラミング言語対応しています  
-([JavaScript](#in-JavaScript-HTML) / [TypeScript](#in-TypeScript-Deno) / [C](#in-C) / [C++](#in-C-1) / [C#](#in-C-2) / [Swift](#in-Swift) / [Java](#in-Java) / [Python](#in-Python) / [Ruby](#in-Ruby) / [PHP](#in-PHP) / [Go](#in-Go) / [Kotlin](#in-Kotlin) / [Dart](#in-Dart) / [Rust](#in-Rust) / [Haskell](#in-haskell-ghc-84x-or-later) / [OpenVBS](#in-OpenVBS) / [Scala](#in-Scala) / [R](#in-R) / [GAS](#in-GAS-Google-App-Script) / [Nim](#in-Nim) / [Lua](#in-Lua) / [Perl](#in-Perl) / [Elixir](#in-Elixir) / [Groovy](#in-Groovy) / [D](#in-D) / [Julia](#in-Julia) / [Racket](#in-Racket) / [OCaml](#in-OCaml) / [Erlang](#in-Erlang) / [Clojure](#in-Clojure) / [F#](#in-F) / [Haxe](#in-Haxe) / [Scheme](#in-Scheme-R6RS) / [Common Lisp](#in-Common-Lisp) / [Elm](#in-Elm) / [Hack](#in-Hack))  
+37 programming languages supported now  
+現在37のプログラミング言語対応しています  
+([JavaScript](#in-JavaScript-HTML) / [TypeScript](#in-TypeScript-Deno) / [C](#in-C) / [C++](#in-C-1) / [C#](#in-C-2) / [Swift](#in-Swift) / [Java](#in-Java) / [Python](#in-Python) / [Ruby](#in-Ruby) / [PHP](#in-PHP) / [Go](#in-Go) / [Kotlin](#in-Kotlin) / [Dart](#in-Dart) / [Rust](#in-Rust) / [Haskell](#in-haskell-ghc-84x-or-later) / [OpenVBS](#in-OpenVBS) / [Scala](#in-Scala) / [R](#in-R) / [GAS](#in-GAS-Google-App-Script) / [Nim](#in-Nim) / [Lua](#in-Lua) / [Perl](#in-Perl) / [Elixir](#in-Elixir) / [Groovy](#in-Groovy) / [D](#in-D) / [Julia](#in-Julia) / [Racket](#in-Racket) / [OCaml](#in-OCaml) / [Erlang](#in-Erlang) / [Clojure](#in-Clojure) / [F#](#in-F) / [Haxe](#in-Haxe) / [Scheme](#in-Scheme-R6RS) / [Common Lisp](#in-Common-Lisp) / [Elm](#in-Elm) / [Hack](#in-Hack) / [PureScript](#in-PureScript))  
 https://taisukef.github.io/Geo3x3/langlist.html  
 
 ## Sample code
@@ -801,6 +801,34 @@ $ brew install hhvm
 to run:
 ```
 $ hhvm simple_geo3x3.hack
+```
+
+### in PureScript
+[Geo3x3.purs](https://github.com/taisukef/Geo3x3/blob/master/purs/src/Geo3x3.purs),
+[Main.purs](https://github.com/taisukef/Geo3x3/blob/master/purs/src/Main.purs)
+```PureScript
+module Main where
+
+import Prelude
+import Effect (Effect)
+import Effect.Console (log)
+
+import Geo3x3 as Geo3x3
+
+main :: Effect Unit
+main = do
+  log (Geo3x3.encode 35.65858 139.745433 14)
+  log (show (Geo3x3.decode "E3793653391822"))
+```
+setup:
+```
+$ spago install integers
+$ spago install strings
+```
+to run:
+```
+$ cd purs
+$ spago run
 ```
 
 
---
+++ purs/src/Geo3x3.purs
@@ -0,0 +1,89 @@
+module Geo3x3 (encode, decode) where
+
+import Prelude (mod, not, show, (&&), (*), (+), (-), (/), (<), (<=), (<>), (==), (>=))
+import Data.Int (toNumber, floor)
+import Data.String (codePointFromChar, length, uncons)
+import Data.Maybe (fromMaybe)
+import Data.Enum (fromEnum)
+
+encode_fn :: String -> Int -> Int -> Number -> Number -> Number -> String
+encode_fn code level i lat lng unit =
+    if i >= level then
+        code
+    else
+        let
+            x = floor (lng / unit)
+            y = floor (lat / unit)
+        in
+        encode_fn
+            (code <> show (1 + x + y * 3))
+            level
+            (i + 1)
+            (lat - toNumber y * unit)
+            (lng - toNumber x * unit)
+            (unit / 3.0)
+
+encode :: Number -> Number -> Int -> String
+encode lat lng level =
+    if level < 1 then
+        ""
+    else
+        encode_fn
+            (if lng >= 0.0 then "E" else "W")
+            level
+            1
+            (90.0 - lat)
+            (if lng >= 0.0 then lng else lng + 180.0)
+            (180.0 / 3.0)
+
+is_1to9 :: Int -> Boolean
+is_1to9 c =
+    c >= 49 && c <= (49 + 9) -- '1' - '9'
+
+decode_fn :: String -> Number -> Number -> Int -> Number -> { lat :: Number, lng :: Number, level :: Int, unit :: Number }
+decode_fn code lat lng level unit =
+    let
+        { head: c, tail: tcode } = fromMaybe { head: codePointFromChar '_', tail: "" } (uncons code)
+        c2 = (fromEnum c)
+    in
+    if not (is_1to9 c2) then
+        let
+            unit0 = unit * 3.0
+        in
+            { lat: 90.0 - (lat + unit0 / 2.0), lng: lng + unit0 / 2.0, level: level, unit: unit0 }
+    else
+        let
+            n = c2 - 49
+        in
+        decode_fn
+            tcode
+            (lat + toNumber (n / 3) * unit)
+            (lng + toNumber (mod n 3) * unit)
+            (level + 1)
+            (unit / 3.0)
+
+decode_fne :: String -> { lat :: Number, lng :: Number, level :: Int, unit :: Number }
+decode_fne code =
+    decode_fn code 0.0 0.0 1 (180.0 / 3.0)
+
+decode_fnw :: String -> { lat :: Number, lng :: Number, level :: Int, unit :: Number }
+decode_fnw code =
+    let
+        pos = decode_fne code
+    in
+    { lat: 180.0 - pos.lat, lng: pos.lng, level: pos.level, unit: pos.unit }
+
+decode :: String -> { lat :: Number, lng :: Number, level :: Int, unit :: Number }
+decode code =
+    if length code == 0 then
+        { lat: 0.0, lng: 0.0, level: 0, unit: 180.0 } -- err
+    else
+        let
+            { head: c, tail: tcode } = fromMaybe { head: codePointFromChar '_', tail: "" } (uncons code)
+        in
+        case fromEnum c of
+            87 -> decode_fnw tcode -- 'W'
+            45 -> decode_fnw tcode -- '-'
+            69 -> decode_fne tcode -- 'E'
+            43 -> decode_fne tcode -- '+'
+            _ -> decode_fne code
