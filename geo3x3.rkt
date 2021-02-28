64442bdcbf405a65bd8209edaf1b8eae83c89c4c
mitsuji <tkms@mitsuji.org>
2021-02-28 09:04:17 +0900

    racket: refactoring

--- geo3x3.rkt
+++ geo3x3.rkt
@@ -13,10 +13,11 @@
                           (let*-values ([(unit1) (/ unit 3)]
                                         [(x y) (values (exact-floor (/ lng unit1))
                                                        (exact-floor (/ lat unit1)))]
-                                        [(c) (integer->char (+ (char->integer #\0) x (* y 3) 1))]
-                                        [(lng1 lat1) (values (- lng (* x unit1))
-                                                             (- lat (* y unit1)))])
-;                             (printf "lng:~a la:~a unit:~a unit1:~a x:~a y:~a x:~a\n" lng lat unit unit1 x y c)
+                                        [(c lng1 lat1) (values (integer->char (+ (char->integer #\0) x (* y 3) 1))
+                                                               (- lng (* x unit1))
+                                                               (- lat (* y unit1)))])
+;                             (printf "lng:~a lat:~a unit:~a unit1:~a x:~a y:~a\n" lng lat unit unit1 x y)
+;                             (printf "c:~a lng1:~a lat1:~a\n" c lng1 lat1)
                               (display c res)
                               (set!-values (lat lng unit) (values lat1 lng1 unit1))))
                       (range 1 level))
@@ -36,13 +37,13 @@
                          (when (< i clen)
                              (let ([n (- (bytes-ref code i) (char->integer #\0))])
                                  (when (and (<= 1 n) (<= n 9))
-                                    (let ([n (- n 1)])
-                                            (let* ([unit1  (/ unit 3)]
-                                                   [lng1   (+ lng (* (remainder n 3) unit1))]
-                                                   [lat1   (+ lat (* (quotient  n 3) unit1))]
-                                                   [level1 (+ level 1)])
-                                                (set!-values (lat lng level unit) (values lat1 lng1 level1 unit1))
-                                                (loop (+ i 1))))))))
+                                      (let*-values ([(unit1) (/ unit 3)]
+                                                    [(lng1 lat1) (let ([n (- n 1)])
+                                                           (values (+ lng (* (remainder n 3) unit1))
+                                                                   (+ lat (* (quotient  n 3) unit1))))]
+                                                    [(level1) (+ level 1)])
+                                          (set!-values (lat lng level unit) (values lat1 lng1 level1 unit1))
+                                          (loop (+ i 1)))))))
                      (list (exact->inexact (let ([lat (+ lat (/ unit 2))]) (- lat 90) ))
                            (exact->inexact (let ([lng (+ lng (/ unit 2))]) (if is-west (- lng 180) lng) ))
                            level
