7703b6d0f4edf4059057e561de5c7fb428e8d8ca
mitsuji <tkms@mitsuji.org>
2021-03-04 14:23:10 +0900

    hackell: Exception handling

--- Geo3x3.hs
+++ Geo3x3.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE BangPatterns #-}
 
 module Geo3x3 ( encode'
+              , decodeE'
               , decode'
               , encode
+              , decodeE
               , decode
               ) where
 
@@ -12,22 +14,26 @@ import qualified Data.ByteString.Builder as Builder
 import qualified Control.Monad.RWS as RWS
 import qualified Control.Monad.State as State
 import Control.Monad (forM_, when)
-import Data.Char (digitToInt)
 import Data.Function ((&))
+import Control.Monad.Except (ExceptT(..),throwError,runExceptT)
 
 type Lat = Double
 type Lng = Double
 type Level = Int
 type Unit = Double
 
+data DecodeError = DecodeEmptyData
+                 | DecodeInvalidDigit (Lat,Lng,Level,Unit)
+                 deriving (Show)
+
 type Encoder =  RWS.RWS () Builder.Builder (Lat,Lng,Unit)
-type Decoder = State.State (Lat,Lng,Level,Unit)
+type Decoder = ExceptT DecodeError (State.State (Lat,Lng,Level,Unit))
 
 encode' :: Lat -> Lng -> Level -> LB8.ByteString
 encode' lat lng level = Builder.toLazyByteString $ snd $ RWS.evalRWS f () (0,0,0) -- (lat,lng,unit)
   where
     f :: Encoder ()
-    f = if level < 1 then error "invalid level"
+    f = if level < 1 then return () -- retuan empty bytes
         else do
 
           let (!c,!lng') = if lng >= 0
@@ -50,11 +56,11 @@ encode' lat lng level = Builder.toLazyByteString $ snd $ RWS.evalRWS f () (0,0,0
                     RWS.tell $! Builder.char7 c 
 
 
-decode' :: B8.ByteString -> (Lat,Lng,Level,Unit)
-decode' code = State.execState f (0,0,1,180) -- (lat,lng,level,unit)
+decodeE' :: B8.ByteString -> Either DecodeError (Lat,Lng,Level,Unit)
+decodeE' code = State.evalState (runExceptT f) (0,0,1,180) -- (lat,lng,level,unit)
   where
-    f :: Decoder ()
-    f = if B8.null code then error "trying to decode empty data"
+    f :: Decoder (Lat,Lng,Level,Unit)
+    f = if B8.null code then throwError DecodeEmptyData
         else do
 
           let (begin,isWest) =
@@ -65,9 +71,9 @@ decode' code = State.execState f (0,0,1,180) -- (lat,lng,level,unit)
 
           let loop = \i ->
                 when (i < B8.length code) $
-                  let n = (digitToInt $ B8.index code i) -- includes hex digits
-                             & \n -> if (n <= 9) then n else error "invalid digit"
-                   in when (n > 0) $ do
+                  let n = (fromEnum $ B8.index code i) - (fromEnum '0')
+                   in if n > 9 then State.get >>= \s -> throwError $ DecodeInvalidDigit s -- halfway result
+                   else when (n > 0) $ do
                      State.modify' $ \(lat,lng,level,unit) ->
                        let !unit' = unit / 3
                            (!lng',!lat') = n - 1 & \n ->
@@ -83,9 +89,21 @@ decode' code = State.execState f (0,0,1,180) -- (lat,lng,level,unit)
                 !lng' = lng + unit / 2 & \lng -> if isWest then lng - 180 else lng
              in (lat',lng',level,unit)
 
+          State.get
+
+
+decode' :: B8.ByteString -> (Lat,Lng,Level,Unit)
+decode' code = either r id $ decodeE' code
+  where
+    r :: DecodeError -> (Lat,Lng,Level,Unit)
+    r DecodeEmptyData = (0,0,0,0)
+    r (DecodeInvalidDigit res) = res -- return halfway result
 
 encode :: Double -> Double -> Int -> String
 encode lat lng level = LB8.unpack $ encode' lat lng level
+
+decodeE :: String -> Either DecodeError (Double,Double,Int,Double)
+decodeE = decodeE' . B8.pack
 
 decode :: String -> (Double,Double,Int,Double)
 decode = decode' . B8.pack
