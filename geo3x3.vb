0040cb77e59f8910197945abfe2b2de78fb49f76
Taisuke Fukuno <fukuno@jig.jp>
2021-03-17 18:49:43 +0900

    improve VB.Net

--- geo3x3.vb
+++ geo3x3.vb
@@ -27,43 +27,44 @@ Public Class Geo3x3
         Dim res(4) As Double
         Dim clen As Integer = Len(code)
         If clen = 0
-            return res
-        end if
+            Return res
+        End If
         Dim begin As Integer = 0
         Dim flg As Boolean = False
         Dim c As String = Mid(code, 1, 1)
-        if c = "-" or c = "W"
+        If c = "-" or c = "W"
             flg = True
             begin = 1
-        else if c = "+" or c = "E"
+        Else If c = "+" or c = "E"
             begin = 1
-        end if
+        End If
         Dim unit As Double = 180.0
         Dim lat As Double = 0.0
         Dim lng As Double = 0.0
         Dim level As Integer = 1
         Dim i As Integer
-        for i = begin to clen - 1
+        For i = begin To clen - 1
             Dim n As Integer
             n = InStr("0123456789", Mid(code, i + 1, 1)) - 1
-            if n > 0
-                unit = unit / 3.0
-                n = n - 1
-                lng += (n mod 3) * unit
-                lat += Math.floor(n / 3) * unit
-                level = level + 1
-            end if
-        next
+            if n <= 0
+                Exit For
+            End If
+            unit /= 3.0
+            n -= 1
+            lng += (n mod 3) * unit
+            lat += Math.Floor(n / 3) * unit
+            level += 1
+        Next
         lat += unit / 2.0
         lng += unit / 2.0
         lat -= 90.0
-        if flg
+        If flg
             lng -= 180.0
-        end if
+        End If
         res(0) = lat
         res(1) = lng
         res(2) = level
         res(3) = unit
-        return res
+        Return res
     End Function
 End Class
