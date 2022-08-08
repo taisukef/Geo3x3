be05580af697b207f172e9e13b9ba2d83685d97d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 18:47:10 +0800

    Delete Simple_geo3x3.hx

--- Simple_geo3x3.hx
+++
@@ -1,8 +0,0 @@
-class Simple_geo3x3 {
-    static public function main(): Void {
-        final code = Geo3x3.encode(35.65858, 139.745433, 14);
-        Sys.println(code);
-        final pos = Geo3x3.decode("E9139659937288");
-        Sys.println(pos);
-    }
-}
