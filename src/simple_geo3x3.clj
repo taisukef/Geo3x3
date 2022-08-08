0d56fb6f8d067b721c4e47f4f7800463f7fa9b2e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 16:53:37 +0800

    Delete src directory

--- src/simple_geo3x3.clj
+++
@@ -1,8 +0,0 @@
-(ns simple_geo3x3
-  (:require [geo3x3])
-)
-(defn -main []
-  (println (geo3x3/encode 35.65858 139.745433 14))
-  (println (geo3x3/decode "E9139659937288"))
-  (println (geo3x3/decode "W9"))
-)
