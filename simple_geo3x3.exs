f9f038d4bb7ac27b785dba7d147214a85b393337
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 21:00:52 +0800

    Delete simple_geo3x3.exs

--- simple_geo3x3.exs
+++
@@ -1,9 +0,0 @@
-Code.require_file("geo3x3.ex")
-
-## get Geo3x3 code from latitude / longitude / level
-code = Geo3x3.encode(35.65858, 139.745433, 14)
-IO.inspect(code)
-
-## get location from Geo3x3 code
-pos = Geo3x3.decode("E9139659937288")
-IO.inspect(pos)
