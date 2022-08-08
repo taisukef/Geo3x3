e9da8a90c2a7c4185645d2d69984285fa8908325
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-09 03:05:51 +0800

    Delete simple_geo3x3.py

--- simple_geo3x3.py
+++
@@ -1,9 +0,0 @@
-import geo3x3
-
-## get Geo3x3 code from latitude / longitude / level
-code = geo3x3.encode(35.65858, 139.745433, 14)
-print(code)
-
-## get location from Geo3x3 code
-pos = geo3x3.decode('E9139659937288')
-print(pos)
