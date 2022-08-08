2fc425bc0a7078616ff3ca7ebeb56fd222acec14
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 21:06:30 +0800

    Delete simple_geo3x3.fs

--- simple_geo3x3.fs
+++
@@ -1,8 +0,0 @@
-module simple_geo3x3
-open Geo3x3
-
-let code = Geo3x3.encode 35.65858 139.745433 14
-printfn "%s" code
-
-let (lat, lng, level, unit) = Geo3x3.decode "E9139659937288"
-printfn "%f %f %d %f" lat lng level unit
