d0a2724f9941c8f35c83e377233f970c00d2113f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 21:36:02 +0800

    Delete simple_geo3x3.ml

--- simple_geo3x3.ml
+++
@@ -1,7 +0,0 @@
-open String;;
-
-let code = Geo3x3.encode 35.65858 139.745433 14;;
-print_endline code;;
-
-let (lat, lng, level, unit) = Geo3x3.decode "E9139659937288";;
-print_endline (String.concat " " [string_of_float lat; string_of_float lng; string_of_int level; string_of_float unit]);;
