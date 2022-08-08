36de06ed9098f6daf15cb2266a3376bbcf24276e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 20:59:33 +0800

    Delete simple_geo3x3.erl

--- simple_geo3x3.erl
+++
@@ -1,7 +0,0 @@
--module(simple_geo3x3).
--export([main/0]).
-
-main() ->
-    io:format("~s~n", [geo3x3:encode(35.65858, 139.745433, 14)]),
-    {Lat, Lng, Level, Unit} = geo3x3:decode("E9139659937288"),
-    io:format("~w ~w ~w ~w~n", [Lat, Lng, Level, Unit]).
