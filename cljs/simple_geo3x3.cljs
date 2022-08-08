292e2a5f4b66fa30695fc7a20a400738582fb0c3
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 16:50:37 +0800

    Delete cljs directory

--- cljs/simple_geo3x3.cljs
+++
@@ -1,7 +0,0 @@
-(ns cljs.simple_geo3x3
-    (:require [cljs.geo3x3 :as geo3x3])
-)
-
-(println (geo3x3/encode 35.65858 139.745433 14))
-(println (geo3x3/decode "E9139659937288"))
-(println (geo3x3/decode "W9"))
