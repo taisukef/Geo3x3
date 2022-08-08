e5ec4735d119146fe85ee8090710566d4088484a
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-08 20:50:25 +0800

    Delete simple_geo3x3.cr

--- simple_geo3x3.cr
+++
@@ -1,9 +0,0 @@
-require "./Geo3x3"
-
-## get Geo3x3 code from latitude / longitude / level
-code = Geo3x3.encode(35.65858, 139.745433, 14)
-puts code
-
-## get location from Geo3x3 code
-pos = Geo3x3.decode("E9139659937288")
-puts pos
