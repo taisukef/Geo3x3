3e51a9824e90cb6f90c0409f15bef7cbee8e4f79
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-09 11:32:36 +0800

    Delete simple_geo3x3.v

--- simple_geo3x3.v
+++
@@ -1,9 +0,0 @@
-import geo3x3
-
-fn main() {
-    code := geo3x3.encode(35.65858, 139.745433, 14)
-    println(code)
-    
-    lat, lng, level, unit := geo3x3.decode("E9139659937288")
-    println('$lat $lng $level $unit')
-}
