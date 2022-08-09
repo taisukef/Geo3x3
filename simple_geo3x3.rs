d9f42e49eec4280c2d85acd27069ead255967427
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-09 10:44:24 +0800

    Delete simple_geo3x3.rs

--- simple_geo3x3.rs
+++
@@ -1,9 +0,0 @@
-mod geo3x3;
-
-fn main() {
-    let res = geo3x3::encode(35.65858, 139.745433, 14);
-    println!("{}", res);
-
-    let pos = geo3x3::decode("E9139659937288".to_string());
-    println!("{} {} {} {}", pos.0, pos.1, pos.2, pos.3);
-
